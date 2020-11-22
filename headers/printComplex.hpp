59c8e1eb5822cae4a3de85fd011052729f61ccde
damixiorek <damixiorek@gmail.com>
2020-11-22 16:27:27 +0100

    add functionality for pairs and maps

--- headers/printComplex.hpp
+++ headers/printComplex.hpp
@@ -1,16 +1,31 @@
 #pragma once
 #include <iostream>
+#include <map>
 #include <type_traits>
+#include <utility>
+#include <vector>
 
+template <typename... T>
 class PrintComplex {
 public:
     template <typename Type, std::enable_if_t<std::is_class<Type>::value, bool> = true>
     PrintComplex& operator()(Type moreThanOneValue)
     {
-        for (const auto& element : moreThanOneValue) {
-            std::cout << element << " ";
+        if constexpr (std::is_same<Type, std::vector<T...>>::value) {
+            for (const auto& element : moreThanOneValue) {
+                std::cout << element << " ";
+            }
+            std::cout << "\n";
         }
-        std::cout << "\n";
+        else if constexpr (std::is_same<Type, std::pair<T...>>::value) {
+            std::cout << moreThanOneValue.first << " " << moreThanOneValue.second << "\n";
+        }
+        else if constexpr (std::is_same<Type, std::map<T...>>::value) {
+            for (const auto& [key, value] : moreThanOneValue) {
+                std::cout << key << " " << value << " ";
+            }
+            std::cout << "\n";
+        }
         return *this;
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <map>
 #include <printComplex.hpp>
+#include <utility>
 #include <vector>
 
 int main()
 {
-    PrintComplex printComplex;
-    std::vector<int> v {10,20,30,40};
-    printComplex(v);
+    std::vector<int> v{10, 20, 30, 40};
+    PrintComplex<int>{}(v);
 
     int num = 999;
-    printComplex(num);
+    PrintComplex<int>{}(num);
+
+    std::pair<std::string, int> p{"boom!", 1000};
+    PrintComplex<std::string, int>{}(p);
+
+    std::map<std::string, int> m{
+        {"shalala!", 777},
+        {"wow!", 111}};
+    PrintComplex<std::string, int>{}(m);
 
     return 0;
 }
